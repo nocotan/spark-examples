0790780acb6512fe9308196a4616955d52c5d049
nocotan <noconoco.lib@gmail.com>
2017-03-11 05:47:33 +0000

    修正

--- examples/NaiveBayesSample.scala
+++ examples/NaiveBayesSample.scala
@@ -1,10 +1,16 @@
 import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
 import org.apache.spark.mllib.util.MLUtils
 
-val data = MLUtils.loadLibSVMFile(sc, "/opt/spark/data/mllib/sample_libsvm_data.txt")
+object NaiveBayesSample {
+  def run() {
+    val data = MLUtils.loadLibSVMFile(sc, "/opt/spark/data/mllib/sample_libsvm_data.txt")
 
-val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
-val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+    val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
+    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
 
-val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
-val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+    println("accuracy: ", accuracy)
+  }
+}
