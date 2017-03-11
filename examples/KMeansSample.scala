b96f1eda64cd9abfd94ae1be0a57742475c589a3
nocotan <noconoco.lib@gmail.com>
2017-03-11 05:43:21 +0000

    修正

--- examples/KMeansSample.scala
+++ examples/KMeansSample.scala
@@ -1,13 +1,17 @@
 import org.apache.spark.ml.clustering.KMeans
 
-val dataset = spark.read.format("libsvm").load("data/mllib/sample_kmeans_data.txt")
+object KMeansSample {
+  def run() {
+    val dataset = spark.read.format("libsvm").load("/opt/spark/data/mllib/sample_kmeans_data.txt")
 
-val kmeans = new KMeans().setK(2).setSeed(1L)
-val model = kmeans.fit(dataset)
+    val kmeans = new KMeans().setK(2).setSeed(1L)
+    val model = kmeans.fit(dataset)
 
-val WSSSE = model.computeCost(dataset)
-println(s"Within Set Sum of Squared Errors = $WSSSE")
+    val WSSSE = model.computeCost(dataset)
+    println(s"Within Set Sum of Squared Errors = $WSSSE")
 
-println("Cluster Centers: ")
-model.clusterCenters.foreach(println)
+    println("Cluster Centers: ")
+    model.clusterCenters.foreach(println)
+  }
+}
 
